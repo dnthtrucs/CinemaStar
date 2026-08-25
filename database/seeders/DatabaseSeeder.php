e66984dc98eb36b6315b63a5cd6eddb967ad3bc4
dnthtrucs <22010313@st.phenikaa-uni.edu.vn>
2026-08-25 19:07:34 +0700

    up

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,25 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::updateOrCreate(['email' => 'admin@cinema.test'], [
-            'name' => 'Quản trị viên',
-            'phone' => '0900000001',
-            'password' => Hash::make('Admin@123'),
-            'role' => 'admin',
-            'is_active' => true,
-            'email_verified_at' => now(),
-        ]);
-
-        User::updateOrCreate(['email' => 'customer@cinema.test'], [
-            'name' => 'Khách hàng Demo',
-            'phone' => '0900000002',
-            'password' => Hash::make('User@123'),
-            'role' => 'customer',
-            'is_active' => true,
-            'email_verified_at' => now(),
-        ]);
-
         $this->call([
+            DemoAccountSeeder::class,
             MovieSeeder::class,
             CinemaSeeder::class,
             RoomSeeder::class,
