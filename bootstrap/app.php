a92eae34d472a3940a0df8cbcc041172771898f0
dnthtrucs <22010313@st.phenikaa-uni.edu.vn>
2026-08-29 23:08:23 +0700

    up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->validateCsrfTokens(except: [
             'payments/momo/ipn',
+            'payments/sepay/webhook',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
